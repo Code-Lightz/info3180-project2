238dfadb727f95f82dd9e8d9eb276b5857b87670
Daniel Battick <danielbattick@gmail.com>
2020-05-20 21:55:22 -0500

    Heroku deploy setup

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,8 @@ from flask_login import LoginManager
 app = Flask(__name__)
 csrf = CSRFProtect(app)
 app.config['SECRET_KEY'] = "v\xf9\xf7\x11\x13\x18\xfaMYp\xed_\xe8\xc9w\x06\x8e\xf0f\xd2\xba\xfd\x8c\xda"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:Christ4life@localhost/Project2"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:Christ4life@localhost/Project2"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://jixgnzupqbhist:5cc730085b0879b543b7fe3a277b2ce5d5021160b82c38a4092d19f91eaf7ea1@ec2-54-165-36-134.compute-1.amazonaws.com:5432/dv7eu77k1tv7n"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 # Flask-Login login manager
